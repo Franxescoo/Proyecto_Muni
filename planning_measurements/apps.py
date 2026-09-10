509ed3cf6557c8047a6abf28d06da41f658c397c
Franxescoo <lucas.godoy06@inacapmail.cl>
2026-09-10 10:24:29 -0300

    Conf. Inicial

---
+++ planning_measurements/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PlanningMeasurementsConfig(AppConfig):
+    name = 'planning_measurements'
---
+++ registration_validation/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RegistrationValidationConfig(AppConfig):
+    name = 'registration_validation'
